2f36f539dfa7e6cd5630edc9fd1aa18a1b31e911
AndreyDid <100933524+AndreyDid@users.noreply.github.com>
2022-12-23 21:13:27 +0300

    Authorization and authentication 2 (Edit page)

--- src/app/components/page/usersListPage/usersListPage.jsx
+++ src/app/components/page/usersListPage/usersListPage.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
-import { paginate } from '../../../utils/paginate'
+import {paginate} from '../../../utils/paginate'
 import Pagination from '../../common/pagination'
 import GroupList from '../../common/groupList'
 import SearchStatus from '../../ui/searchStatus'
 import UserTable from '../../ui/usersTable'
 import SearchField from '../../common/form/searchField'
-import { useUser } from '../../../hooks/useUsers'
-import { useProfessions } from '../../../hooks/useProfession'
+import {useUser} from '../../../hooks/useUsers'
+import {useProfessions} from '../../../hooks/useProfession'
 import _ from 'lodash'
-import { useAuth } from '../../../hooks/useAuth'
+import {useAuth} from '../../../hooks/useAuth'
 
 const UsersListPage = () => {
-    const { users } = useUser()
-    const { currentUser } = useAuth()
-    const { isLoading: professionsLoading, professions } = useProfessions()
+    const {users} = useUser()
+    const {currentUser} = useAuth()
+    const {isLoading: professionsLoading, professions} = useProfessions()
     const [currentPage, setCurrentPage] = useState(1)
     const [selectedProf, setSelectedProf] = useState()
-    const [sortBy, setSortBy] = useState({ path: 'name', order: 'asc' })
+    const [sortBy, setSortBy] = useState({path: 'name', order: 'asc'})
     const [searchQuery, setSearchQuery] = useState('')
     const pageSize = 8
 
@@ -28,7 +28,7 @@ const UsersListPage = () => {
     const handleToggleBookMark = id => {
         const newArray = users.map(user => {
             if (user._id === id) {
-                return { ...user, bookmark: !user.bookmark }
+                return {...user, bookmark: !user.bookmark}
             }
             return user
         })
@@ -52,7 +52,7 @@ const UsersListPage = () => {
         setSortBy(item)
     }
 
-    const handleSearchQuery = ({ target }) => {
+    const handleSearchQuery = ({target}) => {
         setSelectedProf(undefined)
         setSearchQuery(target.value)
     }
@@ -61,18 +61,18 @@ const UsersListPage = () => {
         function filterUsers(data) {
             const filteredUsers = searchQuery
                 ? data.filter(
-                      user =>
-                          user.name
-                              .toLowerCase()
-                              .indexOf(searchQuery.toLowerCase()) !== -1
-                  )
+                    user =>
+                        user.name
+                            .toLowerCase()
+                            .indexOf(searchQuery.toLowerCase()) !== -1
+                )
                 : selectedProf
-                ? data.filter(
-                      user =>
-                          JSON.stringify(user.profession) ===
-                          JSON.stringify(selectedProf)
-                  )
-                : data
+                    ? data.filter(
+                        user =>
+                            JSON.stringify(user.profession) ===
+                            JSON.stringify(selectedProf)
+                    )
+                    : data
             return filteredUsers.filter(u => u._id !== currentUser._id)
         }
 
@@ -107,7 +107,7 @@ const UsersListPage = () => {
                     </div>
                 )}
                 <div className="d-flex flex-column">
-                    <SearchStatus length={count} />
+                    <SearchStatus length={count}/>
                     <SearchField
                         onChange={handleSearchQuery}
                         value={searchQuery}
